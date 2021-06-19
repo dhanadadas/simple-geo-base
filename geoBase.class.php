3849a1b2ac886728c5a4e6d15c1e28254bd74ea5
Dhanada Das <63287949+dhanadadas@users.noreply.github.com>
2021-06-19 17:40:34 +0530

    Adding the main methods to the class geoBase 🍞 #2

--- geoBase.class.php
+++ geoBase.class.php
@@ -16,6 +16,49 @@ class geoBase
 		$this->connect = mysqli_connect($this->sql_host, $this->sql_user, $this->sql_pass, $this->sql_database);
 	}
 
+	public function getCountry()
+	{
+		$country = mysqli_query($this->connect, "SELECT id,name FROM country");
+		return $this->responseJson($country);
+	}
+
+	public function getRegion($country)
+	{
+		if (!isset($country) || !is_numeric($country) || $country <= 0) return false; // is_numeric защита от SQL иньекции.
+
+		$regions = mysqli_query($this->connect, "SELECT id,name FROM region WHERE country_id = $country");
+		return $this->responseJson($regions);
+	}
+
+	public function getCity($region)
+	{
+		if (!isset($region) || !is_numeric($region) || $region <= 0) return false; // is_numeric защита от SQL иньекции.
+
+		$city = mysqli_query($this->connect, "SELECT id,name FROM city WHERE region_id = $region");
+		return $this->responseJson($city);
+	}
+
+	private function responseJson($element)
+	{
+		if (!$element) return false;
+		if ($element->num_rows !== 0) {
+			while ($row = $element->fetch_array(MYSQLI_ASSOC)) {
+				$result[] = $row;
+			}
+		} else return true;//json_encode(array('Нет регионов для этой страны'));
+		return $result;// возвраащем данные в JSON формате;
+	}
+
+	function createDB()
+	{
+		$sql = $this->getSQL();
+		if (mysqli_multi_query($this->connect, $sql)) {
+			return json_encode(['status' => true, 'code' => 'Database created successfully']);
+		} else {
+			return json_encode(['status' => false, 'code' => 'Import error']);
+		}
+	}
+
 	/**
 	 * Метод закрытия соединения
 	 */
